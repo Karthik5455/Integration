df1e9626b8a1c9216715e2008255bb56bcbeb3ee
Karthik5455 <158555209+Karthik5455@users.noreply.github.com>
2024-02-15 23:07:25 -0500

    Add files via upload

---
+++ unit test/integration_test.py
@@ -0,0 +1,8 @@
+import  main
+
+def test_compare():
+     assert main.compare(2, 3) == False
+
+
+def test_celTofr():
+    assert main.celTofr(0)==36
---
+++ unit test/main.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+def compare(num1,num2):
+    if(num1==num2):
+        return True
+    else:
+        return False
+
+def celTofr(num):
+    return num*1.8+32
+
+
